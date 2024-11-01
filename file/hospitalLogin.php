e78fecc518a2202d743d1c772ee5f77cffa00d97
HibahK375 <hibahkhatkhatay@gmail.com>
2024-11-01 23:38:32 +0530

    hibah

--- file/hospitalLogin.php
+++ file/hospitalLogin.php
@@ -1,22 +1,38 @@
 <?php
 session_start();
-    require 'connection.php';
-    if(isset($_POST['hlogin'])){
-    $hemail=$_POST['hemail'];
-    $hpassword=$_POST['hpassword'];
-    $sql="select * from hospitals where hemail='$hemail' and hpassword='$hpassword'";
-    $result=mysqli_query($conn,$sql) or die(mysqli_error($conn));
-    $rows_fetched=mysqli_num_rows($result);
-    if($rows_fetched==0){
-        $error= "Wrong email or password. Please try again.";
-        header( "location:../login.php?error=".$error);
-    }else{
-        $row=mysqli_fetch_array($result);
-        $_SESSION['hemail']=$row['hemail'];
-        $_SESSION['hname']=$row['hname'];
-        $_SESSION['hid']=$row['id'];
-        $msg= $_SESSION['hname'].' have logged in.';
-        header( "location:../hospitalpage.html?msg=".$msg);
-    } 
-  }
+require 'connection.php';
+
+if (isset($_POST['hlogin'])) {
+    // Retrieve input from the form
+    $hid = $_POST['hid'];
+    $hpassword = $_POST['hpassword'];
+
+    // Query to check the provided hospital ID and password
+    $sql = "SELECT * FROM hospitals WHERE hid='$hid'";
+    $result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+
+    if (mysqli_num_rows($result) == 0) {
+        // If no matching ID, redirect with error
+        $error = "Wrong ID or password. Please try again.";
+        header("location:../login.php?error=" . urlencode($error));
+    } else {
+        $row = mysqli_fetch_assoc($result);
+
+        // If passwords are hashed, use password_verify here
+        if ($hpassword === $row['hpassword']) { // Replace with password_verify if hashed
+            // Successful login, set session variables
+            $_SESSION['hemail'] = $row['hemail'];
+            $_SESSION['hname'] = $row['hname'];
+            $_SESSION['hid'] = $row['hid'];
+            
+            // Redirect to hospital page with a success message
+            $msg = $_SESSION['hname'] . ' has logged in.';
+            header("location:../hospitalpage.html?msg=" . urlencode($msg));
+        } else {
+            // Incorrect password, redirect with error
+            $error = "Wrong ID or password. Please try again.";
+            header("location:../login.php?error=" . urlencode($error));
+        }
+    }
+}
 ?>
